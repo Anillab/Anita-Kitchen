658f662520fe7b23bc080d45e9f8a76725c76fbe
Anillab <anillabusolo@gmail.com>
2018-04-29 22:13:35 +0300

    Added three folders,renamed one file

---
+++ JS/anita.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("button#one").click(function(){
+    $("#im1").slideToggle("slow");
+  }
+);
+
+    $("button#two").click(function(){
+      $("#im2").slideToggle("slow");
+    }
+  );
+  $("button#three").click(function(){
+    $("#im3").slideToggle("slow");
+  }
+);
+  $("button#four").click(function(){
+    $("#im4").slideToggle("slow");
+  }
+);
+
+}
+);
